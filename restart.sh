13ce9a3e3e1007d2a775fec6aaf16b3a4f36f62b
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-25 20:13:40 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,14 +1,12 @@
 screen -X -S Quili quit
-apt install unzip
-wget http://95.216.228.91/store.zip
-unzip store.zip
-cd ~/ceremonyclient/node/.config
-rm -rf store
-cd ~
-mv store ~/ceremonyclient/node/.config
+# apt install unzip
+# wget http://95.216.228.91/store.zip
+# unzip store.zip
+# cd ~/ceremonyclient/node/.config
+# rm -rf store
+# cd ~
+# mv store ~/ceremonyclient/node/.config
 
-cd ~/ceremonyclient/node
-git pull
 source /root/.gvm/scripts/gvm
 gvm install go1.4 -B
 gvm use go1.4
@@ -18,4 +16,9 @@ gvm use go1.17.13
 export GOROOT_BOOTSTRAP=$GOROOT
 gvm install go1.20.2
 gvm use go1.20.2
-screen -dmS Quili bash -c './poor_mans_cd.sh'
+
+cd ~/ceremonyclient
+git pull
+git switch release
+cd ~/ceremonyclient/node
+screen -dmS Quili bash -c './release_autorun.sh'
