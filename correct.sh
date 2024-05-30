203847cb0c95206ba7f30f00785baaadee8b6055
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-30 17:13:19 +0800

    Update correct.sh

--- correct.sh
+++ correct.sh
@@ -4,9 +4,20 @@
 screen -X -S Quili quit
 
 cd ~/ceremonyclient
+
+##分配给git 5G的内存，避免拉取代码时报错
+git config --global pack.windowMemory 5g
+git config --global pack.packSizeLimit 5g
+git config --global pack.window 0
+git config --global core.packedGitLimit 5g
+git config --global core.packedGitWindowSize 5g
+
 ##切换至新的git地址
-git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git
+git remote set-url origin https://source.quilibrium.com/quilibrium/ceremonyclient.git.
+
+##拉取最新的代码
 git pull
+
 ##限制每个线程的占用内存数量
 echo 'ulimit -v 640000' >> ~/.bashrc
 source ~/.bashrc
