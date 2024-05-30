70c0412fcc1946d844847162956153833eedefd8
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-30 15:42:18 +0800

    Create server_start.sh

---
+++ server_start.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
+echo "\$nrconf{kernelhints} = 0;" >> /etc/needrestart/needrestart.conf
+echo "\$nrconf{restart} = 'l';" >> /etc/needrestart/needrestart.conf
+
+# 增加swap空间
+sudo mkdir /swap
+sudo fallocate -l 24G /swap/swapfile
+sudo chmod 600 /swap/swapfile
+sudo mkswap /swap/swapfile
+sudo swapon /swap/swapfile
+echo '/swap/swapfile swap swap defaults 0 0' >> /etc/fstab
+
+# 向/etc/sysctl.conf文件追加内容
+echo -e "\n# 自定义最大接收和发送缓冲区大小" >> /etc/sysctl.conf
+echo "net.core.rmem_max=600000000" >> /etc/sysctl.conf
+echo "net.core.wmem_max=600000000" >> /etc/sysctl.conf
+
+echo "配置已添加到/etc/sysctl.conf"
+
+# 重新加载sysctl配置以应用更改
+sysctl -p
+
+echo "sysctl配置已重新加载"
+
+# 更新并升级Ubuntu软件包
+sudo apt update  
+
+# 安装wget、screen和git等组件
+sudo apt -yq install git ufw bison screen binutils gcc make bsdmainutils 
+
+# 安装GVM
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source /root/.gvm/scripts/gvm
+
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.17.13
+gvm use go1.17.13
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.20.2
+gvm use go1.20.2
+
+# 克隆仓库
+git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git
+cd ~/ceremonyclient
+# 切换分支
+git switch release
+# 进入ceremonyclient/node目录
+cd ~/ceremonyclient/node 
+# 赋予执行权限
+chmod +x release_autorun.sh
+
+##通过服务启动
+echo '[Unit]
+Description=Ceremony Client Go App Service
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+WorkingDirectory=/root/ceremonyclient/node
+ExecStart=/root/ceremonyclient/node/release_autorun.sh
+CPUAccounting=true
+CPUQuota=700%
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/ceremonyclient.service
+systemctl daemon-reload
+systemctl start ceremonyclient.service
+journalctl -u ceremonyclient.service -f -n 500
+
+##删除此文件
+cd ~
+rm -f start.sh
