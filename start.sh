61efbb6f924021a0448760fed62c9a52818b8ae0
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-30 18:18:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -63,7 +63,19 @@ cd ~/ceremonyclient/node
 # 赋予执行权限
 chmod +x release_autorun.sh
 # 创建一个screen会话并运行命令
-screen -dmS Quili bash -c 'taskset -c 6 ./release_autorun.sh'
+
+#------------------------计算内存/2的核数，用来运行程序------------------------
+# 获取系统内存大小（单位为 KB）
+total_memory_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+# 将内存大小转换为 GB
+total_memory_gb=$(echo "$total_memory_kb / 1024 / 1024" | bc)
+# 计算内存的一半所需的 CPU 核数
+half_memory_cores=$(echo "($total_memory_gb / 2)" | bc)
+echo "全部的内存: $total_memory_gb GB"
+echo "可使用的核数: $half_memory_cores"
+
+#------------------------启动服务------------------------
+screen -dmS Quili bash -c "taskset -c $half_memory_cores ./release_autorun.sh"
 
 ##同步至最新高度
 # cd ~
