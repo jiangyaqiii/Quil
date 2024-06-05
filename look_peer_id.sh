678a442486d8a891b863a662c22599c2822845b7
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-05 15:50:47 +0800

    Update look_peer_id.sh

--- look_peer_id.sh
+++ look_peer_id.sh
@@ -3,4 +3,9 @@ echo 'flag:v3'
 cd ~/ceremonyclient/node
 ./node-1.4.18-linux-amd64 -peer-id
 cd ~
+echo '查询peer在线情况'
+wget -O - https://raw.githubusercontent.com/0xOzgur/QuilibriumTools/main/visibility_check.sh | bash
+echo ''
+echo '若查询报错，请点击【grpcurl】按键进行修复'
+echo '若查询报错，请点击【grpcurl】按键进行修复'
 rm -f look_peer_id.sh
