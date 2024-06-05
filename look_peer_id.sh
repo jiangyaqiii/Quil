be3329a772787215c6022849cbfae6f82753f7c8
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-06 07:47:43 +0800

    Update look_peer_id.sh

--- look_peer_id.sh
+++ look_peer_id.sh
@@ -6,6 +6,16 @@ cd ~
 echo ''
 echo '========================================='
 echo '查询peer在线情况'
+source /root/.gvm/scripts/gvm
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.17.13
+gvm use go1.17.13
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.20.2
+gvm use go1.20.2
+echo '========================================='
 wget -O - https://raw.githubusercontent.com/0xOzgur/QuilibriumTools/main/visibility_check.sh | bash
 echo ''
 echo '若查询报错，请点击【grpcurl】按键进行修复'
