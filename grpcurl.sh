87f6a3f9ce344b1f72a8b04be9167addd7695029
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-05 15:40:47 +0800

    Create grpcurl.sh

---
+++ grpcurl.sh
@@ -0,0 +1,23 @@
+echo ''
+echo '将go加入全局环境'
+source /root/.gvm/scripts/gvm
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.17.13
+gvm use go1.17.13
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.20.2
+gvm use go1.20.2
+echo '查询go的版本号'
+go -v
+echo ''
+echo '安装grpcurl'
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
+echo ''
+echo '将listenGrpcMultiaddr和listenRESTMultiaddr写入配置文件'
+sed -i 's|listenGrpcMultiaddr: ""|listenGrpcMultiaddr: "/ip4/127.0.0.1/tcp/8337"|' ./ceremonyclient/node/.config/config.yml
+sed -i 's|listenRESTMultiaddr: ""|listenRESTMultiaddr: "/ip4/127.0.0.1/tcp/8338"|' ./ceremonyclient/node/.config/config.yml
+echo ''
+echo '杀死本次quil会话'
+screen -X -S Quili quit
