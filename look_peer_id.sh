89f6ba2fcd4b771cca08b6f34ea97728bba7e58d
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-05 15:51:28 +0800

    Update look_peer_id.sh

--- look_peer_id.sh
+++ look_peer_id.sh
@@ -7,5 +7,5 @@ echo '查询peer在线情况'
 wget -O - https://raw.githubusercontent.com/0xOzgur/QuilibriumTools/main/visibility_check.sh | bash
 echo ''
 echo '若查询报错，请点击【grpcurl】按键进行修复'
-echo '若查询报错，请点击【grpcurl】按键进行修复'
+echo '修复之后，需要等待几分钟，若再次查询仍报错，或显示不在线，请联系客服'
 rm -f look_peer_id.sh
