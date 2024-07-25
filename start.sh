c4582d6dd76979b25b90e088d8eae9e903a47a01
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-25 14:57:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -55,8 +55,8 @@ gvm install go1.20.2
 gvm use go1.20.2
 
 ##引入代理
-# export http_proxy=http://c36812:38683@47.243.191.225:16801
-export http_proxy=$http_proxy
+export http_proxy=http://c36812:38683@47.243.191.225:16801
+# export http_proxy=$http_proxy
 
 # 克隆仓库
 # git clone https://github.com/quilibriumnetwork/ceremonyclient
