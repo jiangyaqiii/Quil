29e4595312a0d22bea4f8447b2d14fe9b015c9c9
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-15 16:48:49 +0800

    Update release2_0_3.sh

--- release2_0_3.sh
+++ release2_0_3.sh
@@ -3,8 +3,8 @@ cd ~/ceremonyclient
 git pull
 git switch v2.0.0-p3
 cd ~/ceremonyclient/node
-screen -dmS Quili bash -c './release_autorun.sh'
+screen -d -m -S Quili bash -c "./node-2.0.0.3-linux-amd64"
 
 ##删除此文件
 cd ~
-rm -f release1_4_18.sh
+rm -f release2_0_3.sh
