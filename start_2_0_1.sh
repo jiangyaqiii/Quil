81256193898eb6507cdfdb004f21f8c6fc2de08e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-24 15:02:05 +0800

    Update start_2_0_1.sh

--- start_2_0_1.sh
+++ start_2_0_1.sh
@@ -40,5 +40,44 @@ cd ~/ceremonyclient
 # 切换分支
 git switch v2.0.0-p3
 # 进入ceremonyclient/node目录
-cd ~/ceremonyclient/node 
 
+cd /root/ceremonyclient/node
+
+# 获取并下载最新的 linux-amd64 文件
+for f in $(curl -s https://releases.quilibrium.com/release | grep linux-amd64); do
+    echo "Processing $f..."
+    
+    # 如果文件存在，删除它
+    if [ -f "$f" ]; then
+        echo "Removing existing file: $f"
+        rm "$f"
+    fi
+    
+    # 下载文件
+    echo "Downloading $f..."
+    curl -s -O "https://releases.quilibrium.com/$f"
+done
+chmod +x node-2*
+
+
+cd /root/ceremonyclient/client
+# 获取并下载最新的 linux-amd64 文件
+for f in $(curl -s https://releases.quilibrium.com/qclient-release | grep linux-amd64); do
+    echo "Processing $f..."
+    
+    # 如果文件存在，删除它
+    if [ -f "$f" ]; then
+        echo "Removing existing file: $f"
+        rm "$f"
+    fi
+    
+    # 下载文件
+    echo "Downloading $f..."
+    curl -s -O "https://releases.quilibrium.com/$f"
+done
+chmod +x qclient-2*
+
+systemctl stop quili_monitor.service
+pkill screen
+cd /root/ceremonyclient/node
+screen -dmS quili bash -c ' ./node-2.0.1-linux-amd64'
