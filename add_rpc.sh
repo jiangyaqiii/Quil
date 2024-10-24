be677b3879e8cf55870f27270db45ebcaf34f35b
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-24 17:45:40 +0800

    Update add_rpc.sh

--- add_rpc.sh
+++ add_rpc.sh
@@ -1,2 +1,2 @@
-sed -i '/listenMultiaddr: /i\  - /ip4/91.242.214.79/udp/8336/quic-v1/p2p/QmNSGavG2DfJwGpHmzKjVmTD6CVSyJsUFTXsW4JXt2eySR' /root/ceremonyclient/node/.config/config.yml.
+sed -i '/listenMultiaddr: /i\  - /ip4/91.242.214.79/udp/8336/quic-v1/p2p/QmNSGavG2DfJwGpHmzKjVmTD6CVSyJsUFTXsW4JXt2eySR' /root/ceremonyclient/node/.config/config.yml
 pkill screen
