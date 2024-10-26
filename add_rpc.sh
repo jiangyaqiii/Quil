b0be6a72ac95dcdfcde357416b51b89066f6a26d
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-26 15:29:07 +0800

    Update add_rpc.sh

--- add_rpc.sh
+++ add_rpc.sh
@@ -1,2 +1,5 @@
+
 sed -i '/listenMultiaddr: /i\  - /ip4/91.242.214.79/udp/8336/quic-v1/p2p/QmNSGavG2DfJwGpHmzKjVmTD6CVSyJsUFTXsW4JXt2eySR' /root/ceremonyclient/node/.config/config.yml
+
+screen -dmS quili bash -c ' ./node-2.0.2-linux-amd64'
 pkill screen
