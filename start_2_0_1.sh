bb9e40ed30ac172aea85f48f82ea8e6b30b5484e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-26 15:25:43 +0800

    Update start_2_0_1.sh

--- start_2_0_1.sh
+++ start_2_0_1.sh
@@ -78,33 +78,34 @@ done
 chmod +x qclient-2*
 
 systemctl stop quili_monitor.service
-pkill screen
+cd /root/ceremonyclient/node
+screen -dmS quili bash -c ' ./node-2.0.2-linux-amd64'
 
-# ===================================公共模块===监控screen模块======================================================================
-cd ~
-#监控screen脚本
-echo '#!/bin/bash
-while true
-do
-    echo "Screen session not found, restarting..."
-    cd /root/ceremonyclient/node
-    screen -dmS quili bash -c ' ./node-2.0.1-linux-amd64'
-    sleep 1800  # 每隔10秒检查一次
-done' > monit.sh
-##给予执行权限
-chmod +x monit.sh
-# ================================================================================================================================
-echo '[Unit]
-Description=Quili Monitor Service
-After=network.target
+# # ===================================公共模块===监控screen模块======================================================================
+# cd ~
+# #监控screen脚本
+# echo '#!/bin/bash
+# while true
+# do
+#     echo "Screen session not found, restarting..."
+#     cd /root/ceremonyclient/node
+#     screen -dmS quili bash -c ' ./node-2.0.2-linux-amd64'
+#     sleep 1800  # 每隔10秒检查一次
+# done' > monit.sh
+# ##给予执行权限
+# chmod +x monit.sh
+# # ================================================================================================================================
+# echo '[Unit]
+# Description=Quili Monitor Service
+# After=network.target
 
-[Service]
-Type=simple
-ExecStart=/bin/bash /root/monit.sh
+# [Service]
+# Type=simple
+# ExecStart=/bin/bash /root/monit.sh
 
-[Install]
-WantedBy=multi-user.target' > /etc/systemd/system/quili_monitor.service
-sudo systemctl daemon-reload
-sudo systemctl enable quili_monitor.service
-sudo systemctl start quili_monitor.service
-sudo systemctl status quili_monitor.service
+# [Install]
+# WantedBy=multi-user.target' > /etc/systemd/system/quili_monitor.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable quili_monitor.service
+# sudo systemctl start quili_monitor.service
+# sudo systemctl status quili_monitor.service
