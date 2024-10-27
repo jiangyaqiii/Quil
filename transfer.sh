5e732eca651a4636db5fbf703873fa868158a436
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-27 20:09:21 +0800

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -6,6 +6,5 @@ cp qclient-2.0.1-linux-amd64.dgst ../node/
 cd ../node
 
 read -p "请输入接收地址: " rev_addr
-read -p "请输入接收地址: " rev_addr
 send_addr=$(./qclient-2.0.1-linux-amd64 token coins)
 ./qclient-2.0.1-linux-amd64 token transfer $send_addr $rev_addr $mount
