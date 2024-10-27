aa06776af7e6ad81deb287df0a618b301a3c0888
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-10-27 22:34:38 +0800

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -4,12 +4,13 @@ echo '显示wquil数量:'
 echo ''
 cp qclient-2.0.1-linux-amd64 ../node/
 cp qclient-2.0.1-linux-amd64.dgst ../node/
-cd ../node
 
 echo '显示发送地址:'
-send_addr=$(./qclient-2.0.1-linux-amd64 token coins)
+./qclient-2.0.1-linux-amd64 token coins --config ~/ceremonyclient/node/.config/
 echo ''
+
 read -p "请输入接收地址: " rev_addr
 read -p "请输入发送地址: " send_addr
-read -p "请输入发送数量: " mount
-./qclient-2.0.1-linux-amd64 token transfer $send_addr $rev_addr $mount
+
+cd ~/ceremonyclient/node/
+./../client/qclient-2.0.1-linux-amd64 token transfer $rev_addr $send_addr
